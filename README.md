718e559f6c388a679b6c146119946c0d28393944
糖饼 <sugarpie.tang@gmail.com>
2017-06-19 16:25:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# [art-template] renderer
+
+[![Build Status](https://travis-ci.org/aui/hexo-renderer-art.svg?branch=master)](https://travis-ci.org/aui/hexo-renderer-art)  [![NPM version](https://badge.fury.io/js/hexo-renderer-art.svg)](http://badge.fury.io/js/hexo-renderer-art) [![Coverage Status](https://img.shields.io/coveralls/aui/hexo-renderer-art.svg)](https://coveralls.io/r/aui/hexo-renderer-art?branch=master)
+
+Add support for [art-template].
+
+## Install
+
+``` bash
+$ npm install hexo-renderer-art --save
+```
+
+[art-template]: https://github.com/aui/art-template
---
+++ test/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var should = require('chai').should(); // eslint-disable-line
+var fs = require('hexo-fs');
+var pathFn = require('path');
+var template = require('art-template');
+
+template.defaults.debug = false;
+template.defaults.bail = true;
+
+describe('art renderer', function() {
+  var r = require('../lib/renderer');
+
+  it('default', function() {
+    var body = 'Hello <%= name %>';
+    var result = r({text: body}, {name: 'world'});
+
+    result.should.eql('Hello world');
+  });
+
+  it('comments', function() {
+    var body = [
+      'Comment <%# hidden %>'
+    ].join('\n');
+
+    var result = r({text: body});
+    result.should.eql('Comment ');
+  });
+
+  it('include', function() {
+    var body = '<% include("./test") %>';
+    var path = pathFn.join(__dirname, 'include_test', 'index.art');
+    var includePath = pathFn.join(path, '../test.art');
+    var includeBody = 'include body';
+
+    return fs.writeFile(includePath, includeBody).then(function() {
+      var result = r({
+        text: body,
+        path: path
+      });
+
+      result.should.eql(includeBody);
+    }).finally(function() {
+      return fs.unlink(includePath);
+    });
+  });
+
+  it('compile', function() {
+    var body = 'Hello <%= name %>';
+    var render = r.compile({text: body});
+    var result = render({
+      name: 'world'
+    });
+
+    result.should.eql('Hello world');
+  });
+});
